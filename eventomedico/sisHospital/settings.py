0e5b025a3c9507d5cafe3182e2ebcc4354266ace
Juan <jm.delgado2@uniandes.edu.co>
2025-05-23 23:57:41 -0500

    Modificado settings con path

--- eventomedico/sisHospital/settings.py
+++ eventomedico/sisHospital/settings.py
@@ -134,4 +134,4 @@ STATICFILES_DIRS = (
 )
 
 PATH_API_GATEWAY = "http://" + os.environ.get("KONG_HOST", "10.128.0.81") + ":" + os.environ.get("KONG_PORT", "8000")
-PATH_VAR = PATH_API_GATEWAY + "/pacientes"
+PATH_PAC = PATH_API_GATEWAY + "/pacientes"
